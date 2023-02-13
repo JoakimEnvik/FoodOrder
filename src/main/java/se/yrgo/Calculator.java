59111475408d059cb4476911edc2ba27071a3297
Tilda Kampman <tilda.kampman@gmail.com>
2023-02-13 15:51:48 +0100

    add documentation to calulateBill

--- src/main/java/se/yrgo/Calculator.java
+++ src/main/java/se/yrgo/Calculator.java
@@ -5,12 +5,18 @@ import java.util.List;
 import java.util.Map;
 
 public class Calculator {
-
-    public static int calculateBill(List<String> bill) {
+    /**
+     * Method that takes a list with an order and compares it to the menu with prices.
+     * when a String from the order matches a String from the menu it takes the price and adds it to the total
+     * then it returns the total sum of the order.
+     * @param order
+     * @return
+     */
+    public static int calculateBill(List<String> order) {
         Menu menu = new Menu();
         int total = 0;
         for (Map.Entry<String, Integer> entry : menu.getFoodMenu().entrySet()) {
-            for (String meal: bill) {
+            for (String meal: order) {
                 if(meal.equals(entry.getKey()))
                     total += entry.getValue();
             }
