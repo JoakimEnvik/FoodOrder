74fc913d6805c3bdb2ee383f52bad419051dc958
JoakimEnvik <joakimekarlsson@gmail.com>
2023-02-13 15:13:54 +0100

    Adding OrderTest.java and cleaning Order.java

--- src/main/java/se/yrgo/Order.java
+++ src/main/java/se/yrgo/Order.java
@@ -4,16 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Order {
-    List<String> order;
+    private List<String> order;
 
     public Order() {
+
         order = new ArrayList<>();
     }
 
     public void addToOrder(String pic) {
+
         order.add(pic);
     }
-    public List getOrder() {
+
+    public List<String> getOrder() {
+
         return order;
     }
 
