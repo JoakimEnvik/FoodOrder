835097eb113414b94a359d59221597e698477137
Tilda Kampman <tilda.kampman@gmail.com>
2023-02-13 11:12:58 +0100

    write test for calculator

--- src/main/java/se/yrgo/Calculator.java
+++ src/main/java/se/yrgo/Calculator.java
@@ -9,17 +9,18 @@ public class Calculator {
 
 
     public static int calculate() {
+        Menu menu = new Menu();
         int total = 0;
-        for (
-                Map.Entry<String, Integer> entry : Menu.foodMenu.entrySet()) {
+        for (Map.Entry<String, Integer> entry : menu.getFoodMenu().entrySet()) {
             total += entry.getValue();
         }
 
         return total;
     }
     public static int calculateBill(List<String> bill) {
+        Menu menu = new Menu();
         int total = 0;
-        for (Map.Entry<String, Integer> entry : Menu.foodMenu.entrySet()) {
+        for (Map.Entry<String, Integer> entry : menu.getFoodMenu().entrySet()) {
             for (String meal: bill) {
                 if(meal.equals(entry.getKey()))
                     total += entry.getValue();
