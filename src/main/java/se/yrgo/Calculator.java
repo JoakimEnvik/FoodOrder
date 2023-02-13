667897402abc9fe1f8cc8c6d25096ec92b3d8727
Tilda Kampman <tilda.kampman@gmail.com>
2023-02-13 11:22:23 +0100

    remove calculate from Calculator

--- src/main/java/se/yrgo/Calculator.java
+++ src/main/java/se/yrgo/Calculator.java
@@ -5,22 +5,11 @@ import java.util.List;
 import java.util.Map;
 
 public class Calculator {
-    private int billNumber;
 
-
-    public static int calculate() {
-        Menu menu = new Menu();
-        int total = 0;
-        for (Map.Entry<String, Integer> entry : menu.getFoodMenu().entrySet()) {
-            total += entry.getValue();
-        }
-
-        return total;
-    }
     public static int calculateBill(List<String> bill) {
         Menu menu = new Menu();
         int total = 0;
-        for (Map.Entry<String, Integer> entry : menu.getFoodMenu().entrySet()) {
+        for (Map.Entry<String, Integer> entry : Menu.foodMenu.entrySet()) {
             for (String meal: bill) {
                 if(meal.equals(entry.getKey()))
                     total += entry.getValue();
