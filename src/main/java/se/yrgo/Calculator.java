3d249c85ec19d1cc97eed1e6d9c8d6542712376c
Tilda Kampman <tilda.kampman@gmail.com>
2023-02-13 15:54:00 +0100

    Calculator Clean up

--- src/main/java/se/yrgo/Calculator.java
+++ src/main/java/se/yrgo/Calculator.java
@@ -1,16 +1,15 @@
 package se.yrgo;
 
-import java.awt.*;
 import java.util.List;
 import java.util.Map;
 
 public class Calculator {
     /**
+     * @param order
      * Method that takes a list with an order and compares it to the menu with prices.
      * when a String from the order matches a String from the menu it takes the price and adds it to the total
      * then it returns the total sum of the order.
-     * @param order
-     * @return
+     * @return order
      */
     public static int calculateBill(List<String> order) {
         Menu menu = new Menu();
