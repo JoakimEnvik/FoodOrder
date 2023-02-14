a269230d23f9f16a4b4efb6db5afba1377446511
Sickan666 <jesabo2001@skola.goteborg.se>
2023-02-14 10:16:28 +0100

    add javadoc comments

--- src/main/java/se/yrgo/Menu.java
+++ src/main/java/se/yrgo/Menu.java
@@ -7,11 +7,19 @@ public class Menu {
 
     private Map <String, Integer> foodMenu = new HashMap<>();
 
+    /**
+     * Constructor wich also calls the method addMeal to add meals to the
+     * HashMap foodMenu
+     */
+
     public Menu() {
         this.foodMenu = foodMenu;
         addMeal();
     }
 
+    /**
+     * Adds dishes and their price to the foodMenu
+     */
     private void addMeal() {
         foodMenu.put("Lasagne", 125);
         foodMenu.put("Pizza", 140);
@@ -21,6 +29,11 @@ public class Menu {
         foodMenu.put("Ice Cream", 69);
     }
 
+    /**
+     * Puts the keys and values from the HashMap into a StringBuilder, using a foorEach-loop,
+     * so it becomes appropriate to use as a presentation of the menu
+     * @return sb.toString  Returns a String format of the StringBuilder
+     */
     public String showMenu() {
         StringBuilder sb = new StringBuilder();
         for (Map.Entry<String, Integer> entry : foodMenu.entrySet()) {
@@ -33,6 +46,10 @@ public class Menu {
         return sb.toString();
     }
 
+    /**
+     * Getter for the foodMenu
+     * @return HashMap foodMenu
+     */
     public Map<String, Integer> getFoodMenu() {
         return foodMenu;
     }
